36edc1f57e2c9e201572cb27c72a18be5a7aa933
denast <denast@proton.me>
2024-07-16 15:33:32 -0400

    [packages] Refactor: categorize package list

--- modules/packages.nix
+++ modules/packages.nix
@@ -2,70 +2,91 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # Eye Candy
     neofetch    # >:P
-    fd          # file search
-    vim         # grandpa
-    wget        # downloading web files
-    neovim      # THE editor
-    ranger      # files manager
+    pfetch      # minimal neofetch alternative
+    bfetch      # utility to center and organize fetch apps
+    dracula-theme # dracula gtk theme
+    dracula-icon-theme # icons for gtk
+
+    # CLI
     kitty       # terminal emulator
     fish        # shell
     starship    # shell prompt
-    git         # versioning software
-    lazygit     # git TUI wrapper
-    ripgrep     # grep optimizer
-    gcc         # C compiler
+    ranger      # files manager
     lshw        # ls hardware
     btop        # resource monitor
+    powertop    # laptop power monitor
     nvtopPackages.full # GPU monitor
+    tmux        # terminal window mux
+    ncdu        # nifty util to see disk space usage
+
+    # OS-Utils
     waybar      # bar for hyprland
-    fusuma      # touchpad gestures
     swww        # wallpapers
     dunst       # notifications
     xfce.thunar # file manager
     wofi        # app runner
+    fusuma      # touchpad gestures
     networkmanagerapplet # network applet
-    tmux        # terminal window mux
     wl-clipboard # copy/paste utility for wayland
-    protonup-qt # util to update Proton GE
-    ncdu        # nifty util to see disk space usage
-    wineWowPackages.stable # wine for windows apps
-    winetricks  # used for more advanced wine settings
-    dracula-theme
-    dracula-icon-theme
     lxqt.lxqt-policykit # polkit for GUI sudo authentication
-    telegram-desktop # messaging
-    webcord     # containerized discord version
-    jq          # json parser
-    xivlauncher # ffxiv oss launcher
     pavucontrol # pulse audio control
-    powertop    # laptop power monitor
     gparted     # partition manager/editor
+    libsForQt5.partitionmanager # gparted alternative??
     xorg.xhost  # gparted doesn't start on wayland without it
     appimage-run # nixos doesn't support the format otherwise
-    localsend   # file sharing app
-    python3     # programming lang
-    libsForQt5.partitionmanager # gparted alternative??
     grim        # grap wayland images
     slurp       # select part of wayland workspace
     swappy      # screenshot tool
-    discord
-    xwaylandvideobridge
-    chromium
-    zip
-    unzip
-    libreoffice-qt6-still
-    openrazer-daemon
-    polychromatic
-    qemu
-    virt-manager
-    wowup-cf
-    obsidian
-    clang-tools
-    bfetch
-    pfetch
+    xwaylandvideobridge # allows screencapture for X.org apps
+    openrazer-daemon # allows to use razer devices
+
+    # CLI-Utils
+    fd          # file search
+    wget        # downloading web files
+    ripgrep     # grep optimizer
+    jq          # json parser
+    zip         # zip archive support
+    unzip       # handy to quickly unpack zips
+
+    # Codin'
+    vim         # grandpa
+    neovim      # THE editor
+    git         # versioning software
+    lazygit     # git TUI wrapper
+    gcc         # C compiler
+    clang-tools # clandg toolkit
+    python3     # Python compiler/interpreter
+
+    # Productivity
+    obsidian    # notes / knowledge base
+    localsend   # file sharing app
+
+    # Gamin'
+    protonup-qt # util to update Proton GE
+    wineWowPackages.stable # wine for windows apps
+    winetricks  # used for more advanced wine settings
+    xivlauncher # ffxiv oss launcher
+    wowup-cf    # WoW addon manager
+
+    # Social
+    telegram-desktop # messaging
+    webcord     # containerized discord web version
+    discord     # when webcord misbehaves
+
+    # Office
+    libreoffice-qt6-still # office suit
+
+    # Misc
+    chromium    # for when firefox doesn't render something right
+    qemu        # virtualization tool
+    virt-manager # qemu gui frontend
+    polychromatic # razer RGB configuration
+
   ];
 
+  # Font definitions
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "Hermit" "JetBrainsMono" ]; })
     font-awesome
