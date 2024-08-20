85758d4890900e9642eea7c6c18e1c39cb973eb1
denast <denast@proton.me>
2024-08-20 11:51:16 -0400

    Firewall settings & some input fixes in modules

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,12 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   networking.networkmanager.enable = true;
   networking.hostName = "denastG14";
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [80 443 53317];
+    allowedUDPPorts = [53317];
+  };
 }
