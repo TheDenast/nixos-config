4abbc703f6cd1947df27040bd64b5b774dbb548d
Konstantin V. <denast@proton.me>
2025-03-05 19:19:13 +0000

    Feat: initial dotfiles import

---
+++ dots/nvim/lua/plugins/others.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+  },
+  {
+    'wakatime/vim-wakatime', lazy = false
+  },
+  {
+      "marcussimonsen/let-it-snow.nvim",
+      cmd = "LetItSnow", -- Wait with loading until command is run
+      opts = {},
+  }
+}
