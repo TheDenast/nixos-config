0ba2e08e09bb7852053b64621eb18454de09c539
denast <denast@proton.me>
2025-04-24 17:03:19 -0400

    [scripts] Feat: tailscale + mullvad VPN/browser automation script

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -10,6 +10,7 @@ alias ll="eza -lh --group-directories-first --icons --no-permissions --no-user"
 alias cnvim='function _cnvim; z $argv; nvim .; end; _cnvim'
 alias mgs='mgitstatus -d 3 --no-upstream -c /home/denast/.nixos-config/ /home/denast/.dots/ | sed \'s/\/home\/denast\///\'; mgitstatus -d 3 --no-upstream -c /home/denast/Pictures/ | sed \'s/\/home\/denast\/Pictures\///\'; mgitstatus -d 3 --no-upstream -c /home/denast/Projects/ | sed \'s/\/home\/denast\/Projects\///\''
 alias sl='sl -w -10 -d'
+alias vpn-browser='bash /home/denast/.dots/scripts/tailscale-vpn-browser.sh'
 setenv EDITOR nvim
 source /home/denast/.secrets/api-keys.fish
 fish_add_path $HOME/.cargo/bin
