dfd24351943719c1a84ddfe69c9380ba888d9b8a
denast <denast@proton.me>
2025-05-29 10:37:37 -0400

    [hypr] Fix: workspaces-restore finds monitor by description, not name

--- dots/hypr/workspaces-restore.sh
+++ dots/hypr/workspaces-restore.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+get_monitor_name() {
+  hyprctl monitors -j | jq -r '.[] | select((.model | type == "string" and contains($search)) or (.description | type == "string" and contains($search))) | .name' --arg search "$1"
+}
+dell_monitor=$(get_monitor_name "DELL")
+
 for i in {1..10}; do
-    hyprctl dispatch moveworkspacetomonitor $i DP-11
-    echo "Moved workspace $i to monitor DP-11"
+    hyprctl dispatch moveworkspacetomonitor $i $dell_monitor
+    echo "Moved workspace $i to monitor  $dell_monitor"
 done
