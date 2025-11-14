28ffd61425900bfe9fceb227c9a1c3c78a087613
denast <denast@proton.me>
2025-11-14 18:30:48 +0400

    [hypr] Fix: update monitor brand in workspaces-restore

--- dots/hypr/workspaces-restore.sh
+++ dots/hypr/workspaces-restore.sh
@@ -3,7 +3,7 @@
 get_monitor_name() {
   hyprctl monitors -j | jq -r '.[] | select((.model | type == "string" and contains($search)) or (.description | type == "string" and contains($search))) | .name' --arg search "$1"
 }
-dell_monitor=$(get_monitor_name "DELL")
+dell_monitor=$(get_monitor_name "Xiaomi")
 
 for i in {1..10}; do
     hyprctl dispatch moveworkspacetomonitor $i $dell_monitor
