610f4a100f9fd79f5ddf62d5beb39e942a3fe60e
denast <denast@proton.me>
2026-08-18 02:06:02 +0400

    Chore: timezone switch

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
   nix.daemonCPUSchedPolicy = "idle";
   nix.daemonIOSchedClass = "idle";
 
-  time.timeZone = "Europe/Belgrade";
+  time.timeZone = "Asia/Yerevan";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
